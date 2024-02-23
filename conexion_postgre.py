a976c4c609b7b29f441fa0a9b839f5d303314680
fjhuete <fjhuete.m@gmail.com>
2024-02-23 22:11:16 +0100

    Añadidos los ficheros para la conexión con Postgre y Oracle

---
+++ conexion_postgre.py
@@ -0,0 +1,58 @@
+import funciones
+
+db = funciones.conexionPS("localhost","proyecto","usuario","proyecto")
+
+funcion=funciones.menu()
+
+while funcion != 7:
+   
+   if funcion == 1:
+      funciones.listar(db)
+      
+      funcion=funciones.menu()
+
+   elif funcion == 2:
+      funciones.buscar(db)
+
+      funcion=funciones.menu()
+   
+   elif funcion == 3:
+      funciones.programadores(db)
+
+      funcion=funciones.menu()
+
+   elif funcion == 4:
+      print("Indica los datos que quieres introducir en la tabla probadores:")
+      dict={}
+      dict["dni"]=input("DNI: ")
+      dict["nombre"]=input("Nombre: ")
+      dict["apellido1"]=input("Primer apellido: ")
+      dict["apellido2"]=input("Segundo apellido: ")
+      dict["telefono"]=input("Teléfono: ")
+      dict["correo"]=input("Correo electrónico: ")
+      funciones.insertar(db,dict)
+
+      funcion = funciones.menu()
+
+   elif funcion == 5:
+      fecha=input("Indica la fecha a partir de la que quieres borrar los registros: ")
+      funciones.borrarPS(db,fecha)
+
+      funcion = funciones.menu()
+
+   elif funcion == 6:
+      print("Indica los datos que quieres modificar en la tabla probadores:")
+      dict={}
+      dict["nombre"]=input("Nombre: ")
+      dict["apellido1"]=input("Primer apellido: ")
+      dict["apellido2"]=input("Segundo apellido: ")
+      dict["telefono"]=input("Nuevo teléfono: ")
+      funciones.actualizar(db,dict)
+
+      funcion = funciones.menu()
+
+   else:
+      print("Error. Por favor indica el número de la opción del menú que quieres ejecutar:")
+      funcion = funciones.menu()
+
+funciones.desconexion(db)

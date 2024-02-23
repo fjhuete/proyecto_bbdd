6365d105c6f10d4a84bd300bb813d72eed91b784
fjhuete <fjhuete.m@gmail.com>
2024-02-23 19:18:03 +0100

    Añadidas las funciones 5 y 6 del programa y corrección de errores en las funciones anteriores

--- conexion_mariadb.py
+++ conexion_mariadb.py
@@ -11,17 +11,44 @@ while funcion != 7:
       
       funcion=funciones.menu()
 
-   if funcion == 2:
+   elif funcion == 2:
       funciones.buscar(db)
 
       funcion=funciones.menu()
    
-   if funcion == 3:
+   elif funcion == 3:
       funciones.programadores(db)
 
       funcion=funciones.menu()
 
-   if funcion == 4:
-      funciones.actualizar(db)
+   elif funcion == 4:
+      print("Indica los datos que quieres introducir en la tabla probadores:")
+      dict={}
+      dict["dni"]=input("DNI: ")
+      dict["nombre"]=input("Nombre: ")
+      dict["apellido1"]=input("Primer apellido: ")
+      dict["apellido2"]=input("Segundo apellido: ")
+      dict["telefono"]=input("Teléfono: ")
+      dict["correo"]=input("Correo electrónico: ")
+      funciones.insertar(db,dict)
 
-      funcion=funciones.menu()
+      funcion = funciones.menu()
+
+   elif funcion == 5:
+      fecha=input("Indica la fecha a partir de la que quieres borrar los registros: ")
+      funciones.borrarMDB(db,fecha)
+
+      funcion = funciones.menu()
+
+   elif funcion == 6:
+      print("Indica los datos que quieres modificar en la tabla probadores:")
+      dict={}
+      dict["nombre"]=input("Nombre: ")
+      dict["apellido1"]=input("Primer apellido: ")
+      dict["apellido2"]=input("Segundo apellido: ")
+      dict["telefono"]=input("Nuevo teléfono: ")
+      funciones.actualizar(db,dict)
+
+      funcion = funciones.menu()
+
+funciones.desconexion(db)
--- funciones.py
+++ funciones.py
@@ -1,4 +1,5 @@
 import sys, MySQLdb, psycopg2
+from datetime import datetime
 
 def conexionMDB(host,usuario,contraseña,nombrebd):
     try:
@@ -46,11 +47,18 @@ def listar(db):
     try:
         cursor.execute(sql)
         datos = cursor.fetchall()
-        print('''
-Nombre  Apellidos               Versión
-========================================''')
+        nombre=0
+        apellidos=0
         for dato in datos:
-            print(dato[0],"     ",dato[1],"     ",dato[2],"     ",dato[3])
+            if nombre < len(dato[0]):
+                nombre=len(dato[0])
+            if apellidos < len(dato[1])+len(dato[2]):
+                apellidos = len(dato[1])+len(dato[2])
+        print('''
+Nombre'''," "*(nombre-3),"Apellidos"," "*(apellidos-6),"Versión",'''
+''',"="*(6+(nombre-3)+9+(apellidos-6)+7+1))
+        for dato in datos:
+            print(dato[0]," "*(nombre-len(dato[0])+3),dato[1],dato[2]," "*(apellidos-len(dato[1])-len(dato[2])+3),dato[3])
     except:
         print("Error en la consulta")
     sql="select * from versiones"
@@ -62,19 +70,36 @@ En total hay %d versiones registradas'''%(cursor.rowcount))
         print("Error en la consulta")
 
 
+
 #2. Indica una versión y muestra todas sus datos
 def buscar(db):
     cod=input("Indica el código de la version que quieres consultar: ")
-    sql="select fechaliberacion,fechacomienzo,nombre,apellido1,apellido2,p.dni from versiones v, programadores p where v.dni = p.dni and v.codigoversion = '%s'"%cod
+    sql="select fechaliberacion,fechacomienzo,nombre,apellido1,apellido2,p.dni from versiones v, programadores p where v.dni = p.dni and v.codigoversion = UPPER('%s')"%cod
     cursor = db.cursor()
     try:
         cursor.execute(sql)
         datos = cursor.fetchall()
-        print('''
-Comienzo                Liberación              Nombre              Apellidos                     DNI
-=============================================================================================================''')
-        for dato in datos:
-            print(dato[0],"     ",dato[1],"     ",dato[2],"     ",dato[3],"     ",dato[4],"     ",dato[5])
+        if cursor.rowcount==0:
+            print("No hay ninguna versión asociada a ese código.")
+        else:
+            comienzo=0
+            liberacion=0
+            nombre=0
+            apellidos=0
+            for dato in datos:
+                if comienzo < len(str(dato[0])):
+                    comienzo=len(str(dato[0]))
+                if liberacion < (len(str(dato[1]))):
+                    liberacion = (len(str(dato[1])))
+                if nombre < len (dato[2]):
+                    nombre = len(dato[2])
+                if apellidos < len(dato[3])+len(dato[4]):
+                    apellidos = len(dato[3])+len(dato[4])
+            print('''
+Comienzo'''," "*(comienzo-5),"Liberación"," "*(liberacion-7),"Nombre"," "*(nombre-3),"Apellidos"," "*(apellidos-6),'''DNI
+''',"="*(8+(comienzo-5)+10+(liberacion-7)+6+(nombre-3)+9+(apellidos-6)+3+1+14))
+            for dato in datos:
+                print(str(dato[0])," "*(comienzo-len(str(dato[0]))+3),dato[1]," "*(liberacion-len(str(dato[1]))+3),dato[2]," "*(nombre-len(dato[2])+3),dato[3],dato[4]," "*(apellidos-len(dato[3])-len(dato[4])+3),dato[5])
     except:
         print("Error en la consulta")
 
@@ -86,14 +111,30 @@ def programadores(db):
     try:
         cursor.execute(sql)
         datos = cursor.fetchall()
-        print('''
-Versión     Nombre      Apellidos           Email       Cantidad
-=================================================================''')
-        for dato in datos:
-            print(dato[0],"     ",dato[1],"     ",dato[2],"     ",dato[3],"     ",dato[4],"     ",dato[5])
+        if cursor.rowcount==0:
+            print("No hay versiones lanzadas en ese año.")
+        else:
+            version=0
+            nombre=0
+            apellidos=0
+            email=0
+            for dato in datos:
+                if version < len(dato[0]):
+                    version = len(dato[0])
+                if nombre < len(dato[1]):
+                    nombre = len(dato[1])
+                if apellidos < len(dato[2])+len(dato[3]):
+                    apellidos = len(dato[2])+len(dato[3])
+                if email < len(dato[4]):
+                    email = len(dato[4])
+            print('''
+Versión'''," "*(version-4),"Nombre"," "*(nombre-3),"Apellidos"," "*(apellidos-6),"Email"," "*(email-2),'''Cantidad
+''',"="*(7+(version-4)+6+(nombre-3)+9+(apellidos-6)+5+(email-2)+8+8))
+            for dato in datos:
+                print(dato[0]," "*(version-len(dato[0])+3),dato[1]," "*(nombre-len(dato[1])+3),dato[2],dato[3]," "*(apellidos-len(dato[2])-len(dato[3])+3),dato[4]," "*(email-len(dato[4])+9),dato[5])
     except:
         print("Error en la consulta")
-
+    
 #4. Insertar información en la tabla probadores
 def insertar(db,probador):
     cursor = db.cursor()
@@ -102,6 +143,52 @@ def insertar(db,probador):
         cursor.execute(sql)
         db.commit()
         print("Los datos de %s %s %s se han añadido con éxito a la tabla probadores."%(probador["nombre"],probador["apellido1"],probador["apellido2"]))
+    except:
+        print("Error al insertar.")
+        db.rollback()
+
+#5. Eliminar las versiones que tengan una fecha de liberación anterior al 28/04/2021.
+def borrarMDB(db,fecha):
+    fecha_dt = datetime.strptime(fecha, '%d/%m/%Y')
+    sql="delete from versiones where FechaLiberacion < '%s'"%fecha_dt
+    cursor = db.cursor()
+    borrar=input("¿Ralmente quieres borrar las versiones anteriores al %r? "%(fecha))
+    if borrar == "Sí" or borrar == "sí" or borrar == "si" or borrar == "Si":
+        try:
+            cursor.execute(sql)
+            db.commit()
+            if cursor.rowcount==0:
+                print("No hay versiones anteriores a esa fecha.")
+            else:
+                print("Se han borrado con éxito las versiones anteriores al %s"%(fecha))
+        except:
+            print("Error al borrar.")
+            db.rollback()
+
+def borrarPS(db,fecha):
+    sql="delete from Versiones where FechaLiberacion < to_date('%s','DD/MM/YYYY')" % (fecha)
+    cursor = db.cursor()
+    borrar=input("¿Ralmente quieres borrar las versiones anteriores al %s? "%(fecha))
+    if borrar == "Sí" or borrar == "sí" or borrar == "si" or borrar == "Si":
+        try:
+            cursor.execute(sql)
+            db.commit()
+            if cursor.rowcount==0:
+                print("No hay versiones anteriores al %s."%(fecha))
+            else:
+                print("Se han borrado con éxito las versiones anteriores al %s"%(fecha))
+        except:
+            print("Error al borrar.")
+            db.rollback()
+
+#6. Actualizar el nuevo teléfono de Paloma Palacio Mansilla: 684846151
+def actualizar(db,probador):
+    sql="update probadores set telefono = '%s' where nombre = '%s' and apellido1 = '%s' and apellido2 = '%s'"%(probador["telefono"],probador["nombre"],probador["apellido1"],probador["apellido2"])
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print("Información actualizada.")
     except:
         print("Error al insertar.")
         db.rollback()
